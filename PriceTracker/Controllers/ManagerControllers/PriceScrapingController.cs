6d1a24636689f43d616b2c440d6a90f062dd26bc
Mateusz Werner <matiwerner20@gmail.com>
2024-07-22 16:00:01 +0200

    Wprowadzenie poprawek do scrapowania

--- PriceTracker/Controllers/ManagerControllers/PriceScrapingController.cs
+++ PriceTracker/Controllers/ManagerControllers/PriceScrapingController.cs
@@ -63,7 +63,7 @@ public class PriceScrapingController : Controller
         var actualRejectedProducts = new List<ProductClass>();
         var stopwatch = new Stopwatch();
         var tasks = new List<Task>();
-        var semaphore = new SemaphoreSlim(8);
+        var semaphore = new SemaphoreSlim(5);
 
         stopwatch.Start();
 
