434c763694286ee9fcda99f65cbe8b491a58ba1f
Mateusz Werner <matiwerner20@gmail.com>
2024-07-29 15:57:05 +0200

    Dodanie selektywnego wyboru scrapowania dla danego sklepu

--- PriceTracker/Controllers/MemberControllers/CompetitorsController.cs
+++ PriceTracker/Controllers/MemberControllers/CompetitorsController.cs
@@ -1,6 +1,9 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using PriceTracker.Data;
+using PriceTracker.Models;
+using System.Linq;
+using System.Threading.Tasks;
 
 public class CompetitorsController : Controller
 {
@@ -11,15 +14,15 @@ public class CompetitorsController : Controller
         _context = context;
     }
 
-
+   
     public async Task<IActionResult> Index()
     {
         var stores = await _context.Stores.ToListAsync();
-      
         return View("~/Views/Panel/Competitors/Index.cshtml", stores);
     }
 
-    
+
+
     public async Task<IActionResult> Competitors(int storeId)
     {
         var storeName = await _context.Stores
@@ -28,7 +31,7 @@ public class CompetitorsController : Controller
             .FirstOrDefaultAsync();
 
         var competitors = await _context.PriceHistories
-            .Where(ph => ph.Product.StoreId == storeId) 
+            .Where(ph => ph.Product.StoreId == storeId)
             .Select(ph => new { ph.StoreName, ph.ProductId })
             .Distinct()
             .GroupBy(ph => ph.StoreName)
@@ -42,47 +45,65 @@ public class CompetitorsController : Controller
             .ToListAsync();
 
         ViewBag.StoreName = storeName;
-
+        ViewBag.StoreId = storeId;
         return View("~/Views/Panel/Competitors/Competitors.cshtml", competitors);
-      
     }
 
 
-
-
-    // Metoda wyświetlająca listę scrapowań dla wybranego konkurenta
-    public async Task<IActionResult> CompetitorScrapings(string competitorStoreName, int storeId)
+    public async Task<IActionResult> CompetitorPrices(int storeId, string competitorStoreName)
     {
-        var scrapHistories = await _context.ScrapHistories
-            .Where(sh => sh.PriceHistories.Any(ph => ph.StoreName == competitorStoreName))
-            .OrderByDescending(sh => sh.Date)
-            .ToListAsync();
-
         ViewBag.CompetitorStoreName = competitorStoreName;
         ViewBag.StoreId = storeId;
-     
-        return View("~/Views/Panel/Competitors/CompetitorScrapings.cshtml", scrapHistories);
+        return View("~/Views/Panel/Competitors/CompetitorPrices.cshtml");
     }
 
-    // Metoda wyświetlająca ceny produktów dla wybranego scrapowania
-    public async Task<IActionResult> CompetitorPrices(int scrapHistoryId, string competitorStoreName)
+    public async Task<IActionResult> GetScrapHistoryIds(int storeId)
     {
+        var scrapHistoryIds = await _context.ScrapHistories
+            .Where(sh => sh.StoreId == storeId)
+            .OrderByDescending(sh => sh.Date)
+            .Select(sh => new { sh.Id, sh.Date })
+            .ToListAsync();
+
+        return Json(scrapHistoryIds);
+    }
+    public class GetCompetitorPricesRequest
+    {
+        public int StoreId { get; set; }
+        public string CompetitorStoreName { get; set; }
+        public int ScrapHistoryId { get; set; }
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> GetCompetitorPrices([FromBody] GetCompetitorPricesRequest request)
+    {
+        Console.WriteLine($"Received request for competitor prices with parameters: storeId={request.StoreId}, competitorStoreName={request.CompetitorStoreName}, scrapHistoryId={request.ScrapHistoryId}");
+
         var prices = await _context.PriceHistories
-            .Where(ph => ph.ScrapHistoryId == scrapHistoryId && ph.StoreName == competitorStoreName)
+            .Where(ph => ph.ScrapHistoryId == request.ScrapHistoryId && ph.StoreName == request.CompetitorStoreName)
             .Select(ph => new
             {
+                ph.ProductId,
                 ph.Product.ProductName,
                 ph.Price,
-                ph.Position,
-                ph.IsBidding,
-                ph.AvailabilityNum,
-                ph.ShippingCostNum
+                ph.ScrapHistoryId
             })
             .ToListAsync();
 
-        ViewBag.CompetitorStoreName = competitorStoreName;
-        ViewBag.ScrapHistoryId = scrapHistoryId;
+        Console.WriteLine($"Found {prices.Count} prices for competitor {request.CompetitorStoreName} in scrap history {request.ScrapHistoryId}");
 
-        return View("~/Views/Panel/Competitors/CompetitorPrices.cshtml", prices);
+        foreach (var price in prices)
+        {
+            Console.WriteLine($"ProductId: {price.ProductId}, ProductName: {price.ProductName}, Price: {price.Price}, ScrapHistoryId: {price.ScrapHistoryId}");
+        }
+
+        return Json(prices);
     }
+
+
+
+
+
+
+
 }
