5c0b04a83294f0a29c23b5b9b189e99c637f50a4
Mateusz Werner <matiwerner20@gmail.com>
2024-09-18 23:03:44 +0200

    Poprawki i umiejetnosc poprawnego rozijania i znajdowania wszystkich ofert i wybranie tej najlepszej

--- PriceSafari/Models/GooglePriceScraper.cs
+++ PriceSafari/Models/GooglePriceScraper.cs
@@ -40,17 +40,19 @@ namespace PriceSafari.Services
             //await _page.SetJavaScriptEnabledAsync(settings.JavaScript);
             Console.WriteLine("Przeglądarka zainicjalizowana.");
         }
+
+        
+
         public async Task<List<PriceData>> ScrapePricesAsync(GoogleScrapingProduct scrapingProduct)
         {
             var scrapedData = new List<PriceData>();
-            var seenStoreNames = new HashSet<string>();
+            var storeBestOffers = new Dictionary<string, PriceData>(); // Store the best offers for each store
 
             // Pobieranie identyfikatora produktu z URL
             string productId = ExtractProductId(scrapingProduct.GoogleUrl);
 
             // Tworzenie URL z "/offers" oraz dynamicznym parametrem ?prds=cid:<productId>,cond:1
             string productOffersUrl = $"{scrapingProduct.GoogleUrl}/offers?prds=cid:{productId},cond:1";
-            string googleBaseUrl = "https://www.google.com";
             bool hasNextPage = true;
             int totalOffersCount = 0;
             int currentPage = 0;
@@ -66,6 +68,19 @@ namespace PriceSafari.Services
                     await _page.GoToAsync(paginatedUrl, new NavigationOptions { WaitUntil = new[] { WaitUntilNavigation.Networkidle2 } });
                     await Task.Delay(50);
 
+                    // Sprawdzenie i kliknięcie przycisków rozwijających oferty
+                    var moreOffersButtons = await _page.QuerySelectorAllAsync("div.cNMlI");
+                    if (moreOffersButtons.Length > 0)
+                    {
+                        foreach (var button in moreOffersButtons)
+                        {
+                            Console.WriteLine("Znaleziono przycisk 'Jeszcze oferty'. Klikam, aby rozwinąć.");
+                            await button.ClickAsync();
+                            await Task.Delay(500); // Czekamy, aż oferty się załadują
+                        }
+                    }
+
+                    // Zbieranie ofert podstawowych
                     var offerRowsSelector = "#sh-osd__online-sellers-cont > tr";
                     var offerRows = await _page.QuerySelectorAllAsync(offerRowsSelector);
                     var offersCount = offerRows.Length;
@@ -79,6 +94,7 @@ namespace PriceSafari.Services
 
                     Console.WriteLine($"Znaleziono {offersCount} ofert. Rozpoczynam scrapowanie...");
 
+                    // Zbieranie normalnych ofert
                     for (int i = 1; i <= offersCount; i++)
                     {
                         var storeNameSelector = $"#sh-osd__online-sellers-cont > tr:nth-child({i}) > td:nth-child(1) > div.kPMwsc > a";
@@ -92,24 +108,43 @@ namespace PriceSafari.Services
                             var storeName = await storeNameElement.EvaluateFunctionAsync<string>("node => node.firstChild.textContent.trim()");
                             Console.WriteLine($"Znaleziono sklep: {storeName}");
 
-                            if (!seenStoreNames.Contains(storeName))
+                            var priceElement = await _page.QuerySelectorAsync(priceSelector);
+                            var priceText = priceElement != null ? await priceElement.EvaluateFunctionAsync<string>("node => node.textContent.trim()") : "Brak ceny";
+                            Console.WriteLine($"Cena: {priceText}");
+
+                            var priceWithDeliveryElement = await _page.QuerySelectorAsync(priceWithDeliverySelector);
+                            var priceWithDeliveryText = priceWithDeliveryElement != null ? await priceWithDeliveryElement.EvaluateFunctionAsync<string>("node => node.textContent.trim()") : priceText;
+                            Console.WriteLine($"Cena z dostawą: {priceWithDeliveryText}");
+
+                            var priceDecimal = ExtractPrice(priceText);
+                            var priceWithDeliveryDecimal = ExtractPrice(priceWithDeliveryText);
+
+                            var offerUrlElement = await _page.QuerySelectorAsync(offerUrlSelector);
+                            var offerUrl = offerUrlElement != null ? await offerUrlElement.EvaluateFunctionAsync<string>("node => node.href") : "Brak URL";
+                            Console.WriteLine($"URL oferty: {offerUrl}");
+
+                            // Sprawdzenie, czy oferta dla sklepu już istnieje
+                            if (storeBestOffers.ContainsKey(storeName))
                             {
-                                var priceElement = await _page.QuerySelectorAsync(priceSelector);
-                                var priceText = priceElement != null ? await priceElement.EvaluateFunctionAsync<string>("node => node.textContent.trim()") : "Brak ceny";
-                                Console.WriteLine($"Cena: {priceText}");
-
-                                var priceWithDeliveryElement = await _page.QuerySelectorAsync(priceWithDeliverySelector);
-                                var priceWithDeliveryText = priceWithDeliveryElement != null ? await priceWithDeliveryElement.EvaluateFunctionAsync<string>("node => node.textContent.trim()") : priceText;
-                                Console.WriteLine($"Cena z dostawą: {priceWithDeliveryText}");
-
-                                var priceDecimal = ExtractPrice(priceText);
-                                var priceWithDeliveryDecimal = ExtractPrice(priceWithDeliveryText);
-
-                                var offerUrlElement = await _page.QuerySelectorAsync(offerUrlSelector);
-                                var offerUrl = offerUrlElement != null ? await offerUrlElement.EvaluateFunctionAsync<string>("node => node.href") : "Brak URL";
-                                Console.WriteLine($"URL oferty: {offerUrl}");
-
-                                scrapedData.Add(new PriceData
+                                var existingOffer = storeBestOffers[storeName];
+                                if (priceWithDeliveryDecimal < existingOffer.PriceWithDelivery)
+                                {
+                                    // Zastępujemy istniejącą ofertę, jeśli nowa ma niższą cenę z dostawą
+                                    storeBestOffers[storeName] = new PriceData
+                                    {
+                                        StoreName = storeName,
+                                        Price = priceDecimal,
+                                        PriceWithDelivery = priceWithDeliveryDecimal,
+                                        OfferUrl = offerUrl,
+                                        ScrapingProductId = scrapingProduct.ScrapingProductId,
+                                        RegionId = scrapingProduct.RegionId
+                                    };
+                                }
+                            }
+                            else
+                            {
+                                // Dodajemy nową ofertę do słownika
+                                storeBestOffers[storeName] = new PriceData
                                 {
                                     StoreName = storeName,
                                     Price = priceDecimal,
@@ -117,13 +152,7 @@ namespace PriceSafari.Services
                                     OfferUrl = offerUrl,
                                     ScrapingProductId = scrapingProduct.ScrapingProductId,
                                     RegionId = scrapingProduct.RegionId
-                                });
-
-                                seenStoreNames.Add(storeName);
-                            }
-                            else
-                            {
-                                Console.WriteLine($"Sklep {storeName} już zebrany.");
+                                };
                             }
                         }
                         else
@@ -132,7 +161,80 @@ namespace PriceSafari.Services
                         }
                     }
 
-                    // Sprawdzenie, czy istnieje kolejna strona nawigacyjna
+                    // Zbieranie ofert ukrytych po rozwinięciu
+                    var hiddenOfferRowsSelector = "tr.sh-osd__offer-row[data-hveid][data-is-grid-offer='true']";
+                    var hiddenOfferRows = await _page.QuerySelectorAllAsync(hiddenOfferRowsSelector);
+                    for (int j = 0; j < hiddenOfferRows.Length; j++)
+                    {
+                        var hiddenRowElement = hiddenOfferRows[j];
+
+                        // Selektor do pobierania nazwy sklepu
+                        var hiddenStoreNameSelector = "td:nth-child(1) > div._-ez > a";
+                        var hiddenPriceSelector = "td:nth-child(4) > span";
+                        var hiddenPriceWithDeliverySelector = "td:nth-child(5) > div";
+                        var hiddenOfferUrlSelector = "td:nth-child(6) > div > a";
+
+                        var hiddenStoreNameElement = await hiddenRowElement.QuerySelectorAsync(hiddenStoreNameSelector);
+                        if (hiddenStoreNameElement != null)
+                        {
+                            // Pobieranie tekstu sklepu, wyłączając <span> (przez `firstChild`)
+                            var hiddenStoreName = await hiddenStoreNameElement.EvaluateFunctionAsync<string>("node => node.firstChild.textContent.trim()");
+                            Console.WriteLine($"Znaleziono ukryty sklep: {hiddenStoreName}");
+
+                            var hiddenPriceElement = await hiddenRowElement.QuerySelectorAsync(hiddenPriceSelector);
+                            var hiddenPriceText = hiddenPriceElement != null ? await hiddenPriceElement.EvaluateFunctionAsync<string>("node => node.textContent.trim()") : "Brak ceny";
+                            Console.WriteLine($"Ukryta cena: {hiddenPriceText}");
+
+                            var hiddenPriceWithDeliveryElement = await hiddenRowElement.QuerySelectorAsync(hiddenPriceWithDeliverySelector);
+                            var hiddenPriceWithDeliveryText = hiddenPriceWithDeliveryElement != null ? await hiddenPriceWithDeliveryElement.EvaluateFunctionAsync<string>("node => node.textContent.trim()") : hiddenPriceText;
+                            Console.WriteLine($"Ukryta cena z dostawą: {hiddenPriceWithDeliveryText}");
+
+                            var hiddenPriceDecimal = ExtractPrice(hiddenPriceText);
+                            var hiddenPriceWithDeliveryDecimal = ExtractPrice(hiddenPriceWithDeliveryText);
+
+                            var hiddenOfferUrlElement = await hiddenRowElement.QuerySelectorAsync(hiddenOfferUrlSelector);
+                            var hiddenOfferUrl = hiddenOfferUrlElement != null ? await hiddenOfferUrlElement.EvaluateFunctionAsync<string>("node => node.href") : "Brak URL";
+                            Console.WriteLine($"Ukryty URL oferty: {hiddenOfferUrl}");
+
+                            // Dodanie do logu informacji o znalezionej ofercie ukrytej
+                            Console.WriteLine($"Oferta ukryta -> Sklep: {hiddenStoreName}, Cena: {hiddenPriceText}, URL: {hiddenOfferUrl}");
+
+                            // Sprawdzenie, czy oferta ukryta dla tego sklepu jest lepsza
+                            if (storeBestOffers.ContainsKey(hiddenStoreName))
+                            {
+                                var existingOffer = storeBestOffers[hiddenStoreName];
+                                if (hiddenPriceWithDeliveryDecimal < existingOffer.PriceWithDelivery)
+                                {
+                                    // Zastępujemy ofertę, jeśli ukryta oferta ma niższą cenę z dostawą
+                                    storeBestOffers[hiddenStoreName] = new PriceData
+                                    {
+                                        StoreName = hiddenStoreName,
+                                        Price = hiddenPriceDecimal,
+                                        PriceWithDelivery = hiddenPriceWithDeliveryDecimal,
+                                        OfferUrl = hiddenOfferUrl,
+                                        ScrapingProductId = scrapingProduct.ScrapingProductId,
+                                        RegionId = scrapingProduct.RegionId
+                                    };
+                                }
+                            }
+                            else
+                            {
+                                // Dodajemy ukrytą ofertę, jeśli nie ma jeszcze żadnej dla tego sklepu
+                                storeBestOffers[hiddenStoreName] = new PriceData
+                                {
+                                    StoreName = hiddenStoreName,
+                                    Price = hiddenPriceDecimal,
+                                    PriceWithDelivery = hiddenPriceWithDeliveryDecimal,
+                                    OfferUrl = hiddenOfferUrl,
+                                    ScrapingProductId = scrapingProduct.ScrapingProductId,
+                                    RegionId = scrapingProduct.RegionId
+                                };
+                            }
+                        }
+                    }
+
+
+                    // Sprawdzenie, czy istnieje kolejna strona
                     var paginationElement = await _page.QuerySelectorAsync("#sh-fp__pagination-button-wrapper");
                     if (paginationElement != null)
                     {
@@ -158,6 +260,8 @@ namespace PriceSafari.Services
                     await Task.Delay(100);
                 }
 
+                // Dodajemy wszystkie najlepsze oferty do listy scrapedData
+                scrapedData.AddRange(storeBestOffers.Values);
                 Console.WriteLine($"Zakończono przetwarzanie {scrapedData.Count} ofert.");
             }
             catch (Exception ex)
