6cd4b6d020b01c10ef22321168486b45d16f04fc
Mateusz Werner <matiwerner20@gmail.com>
2025-01-31 15:26:08 +0100

    Dodanie przechowywania statusów maszyn

---
+++ PriceSafari/Controllers/ManagerControllers/DeviceStatusController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using PriceSafari.Data;
+
+namespace PriceSafari.Controllers.ManagerControllers
+{
+    [Authorize(Roles = "Admin")]
+    public class DeviceStatusController : Controller
+    {
+        private readonly PriceSafariContext _context;
+
+        public DeviceStatusController(PriceSafariContext context)
+        {
+            _context = context;
+        }
+
+      
+        [HttpGet]
+        public async Task<IActionResult> DeviceStatus()
+        {
+            // Pobieramy wszystkie wpisy, sortujemy od najnowszego do najstarszego
+            var deviceStatuses = await _context.DeviceStatuses
+                .OrderByDescending(d => d.LastCheck)
+                .ToListAsync();
+
+          
+            return View("~/Views/ManagerPanel/Settings/DeviceStatus.cshtml", deviceStatuses);
+        }
+
+    
+        [HttpPost]
+        public async Task<IActionResult> ClearAll()
+        {
+            var allRecords = _context.DeviceStatuses.ToList();
+            _context.DeviceStatuses.RemoveRange(allRecords);
+            await _context.SaveChangesAsync();
+
+            return RedirectToAction(nameof(DeviceStatus));
+        }
+    }
+}
