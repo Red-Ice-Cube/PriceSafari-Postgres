77b17b51cb2e1b7b3c19b42f404213b02d431387
Mateusz Werner <matiwerner20@gmail.com>
2025-10-01 20:53:11 +0200

    Usuniecie parametru Google Mig

---
+++ PriceSafari/Migrations/20251001185108_blankming.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PriceSafari.Migrations
+{
+    /// <inheritdoc />
+    public partial class blankming : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "GoogleMiG",
+                table: "Stores");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "GoogleMiG",
+                table: "Stores",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+    }
+}
