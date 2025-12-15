9c65516a06b81d8f1cbc58a91e28cc85b0d2db65
Mateusz Werner <matiwerner20@gmail.com>
2025-12-15 11:15:12 +0100

    Dodanie zmian w widoku dla trybu allegro

---
+++ PriceSafari/Migrations/20251215100931_addnewvaluesallegromodeselected.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PriceSafari.Migrations
+{
+    /// <inheritdoc />
+    public partial class addnewvaluesallegromodeselected : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Mode",
+                table: "AllegroPriceBridgeItems",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<decimal>(
+                name: "PriceIndexTarget",
+                table: "AllegroPriceBridgeItems",
+                type: "decimal(18,2)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<decimal>(
+                name: "StepPriceApplied",
+                table: "AllegroPriceBridgeItems",
+                type: "decimal(18,2)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Mode",
+                table: "AllegroPriceBridgeItems");
+
+            migrationBuilder.DropColumn(
+                name: "PriceIndexTarget",
+                table: "AllegroPriceBridgeItems");
+
+            migrationBuilder.DropColumn(
+                name: "StepPriceApplied",
+                table: "AllegroPriceBridgeItems");
+        }
+    }
+}
