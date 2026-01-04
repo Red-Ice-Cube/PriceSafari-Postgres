39544c9d83d883a484d43a0eff0af9df3f33485f
Mateusz Werner <matiwerner20@gmail.com>
2026-01-04 13:53:14 +0100

    Dodanie rozszerzonego modelu allegro o faktyczne id oferty wyciagniee z url

---
+++ PriceSafari/Migrations/20260104125216_enchanedallegromodel.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PriceSafari.Migrations
+{
+    /// <inheritdoc />
+    public partial class enchanedallegromodel : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "IdOnAllegro",
+                table: "AllegroProducts",
+                type: "nvarchar(50)",
+                maxLength: 50,
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IdOnAllegro",
+                table: "AllegroProducts");
+        }
+    }
+}
