3becc816b5584f407cc0e835778a3f66857965c8
Mateusz Werner <matiwerner20@gmail.com>
2026-01-04 14:40:45 +0100

    Dodanie nowych zminnych informaujachych o liczbie pod obn i utrzy na przesterzeni batch auto

--- PriceSafari/Migrations/20260104133955_dodaniezmiennych o podwyzkach obnizkach i utrzymaniu celu dla danej reguly automatyzacji.Designer.cs
+++ PriceSafari/Migrations/20260104133955_dodaniezmiennych o podwyzkach obnizkach i utrzymaniu celu dla danej reguly automatyzacji.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using PriceSafari.Data;
 
@@ -11,9 +12,11 @@ using PriceSafari.Data;
 namespace PriceSafari.Migrations
 {
     [DbContext(typeof(PriceSafariContext))]
-    partial class PriceSafariContextModelSnapshot : ModelSnapshot
+    [Migration("20260104133955_dodaniezmiennych o podwyzkach obnizkach i utrzymaniu celu dla danej reguly automatyzacji")]
+    partial class dodaniezmiennychopodwyzkachobnizkachiutrzymaniuceludladanejregulyautomatyzacji
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ PriceSafari/Migrations/20260104133955_dodaniezmiennych o podwyzkach obnizkach i utrzymaniu celu dla danej reguly automatyzacji.cs
@@ -0,0 +1,78 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PriceSafari.Migrations
+{
+    /// <inheritdoc />
+    public partial class dodaniezmiennychopodwyzkachobnizkachiutrzymaniuceludladanejregulyautomatyzacji : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "PriceDecreasedCount",
+                table: "PriceBridgeBatches",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "PriceIncreasedCount",
+                table: "PriceBridgeBatches",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "PriceMaintainedCount",
+                table: "PriceBridgeBatches",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "PriceDecreasedCount",
+                table: "AllegroPriceBridgeBatches",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "PriceIncreasedCount",
+                table: "AllegroPriceBridgeBatches",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "PriceMaintainedCount",
+                table: "AllegroPriceBridgeBatches",
+                type: "int",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "PriceDecreasedCount",
+                table: "PriceBridgeBatches");
+
+            migrationBuilder.DropColumn(
+                name: "PriceIncreasedCount",
+                table: "PriceBridgeBatches");
+
+            migrationBuilder.DropColumn(
+                name: "PriceMaintainedCount",
+                table: "PriceBridgeBatches");
+
+            migrationBuilder.DropColumn(
+                name: "PriceDecreasedCount",
+                table: "AllegroPriceBridgeBatches");
+
+            migrationBuilder.DropColumn(
+                name: "PriceIncreasedCount",
+                table: "AllegroPriceBridgeBatches");
+
+            migrationBuilder.DropColumn(
+                name: "PriceMaintainedCount",
+                table: "AllegroPriceBridgeBatches");
+        }
+    }
+}
