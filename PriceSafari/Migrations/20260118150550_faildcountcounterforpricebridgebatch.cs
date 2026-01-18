9996a15e612cc23613f391b5342a55ee8b052d87
Mateusz Werner <matiwerner20@gmail.com>
2026-01-18 16:06:48 +0100

    rozszerzenie obiektu pricebridgebatch

---
+++ PriceSafari/Migrations/20260118150550_faildcountcounterforpricebridgebatch.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PriceSafari.Migrations
+{
+    /// <inheritdoc />
+    public partial class faildcountcounterforpricebridgebatch : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "FailedCount",
+                table: "PriceBridgeBatches",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "FailedCount",
+                table: "PriceBridgeBatches");
+        }
+    }
+}
