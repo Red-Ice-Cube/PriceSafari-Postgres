fcdec93163dbb00c79bec4426257230b7cf9a9a0
Mateusz Werner <matiwerner20@gmail.com>
2026-03-03 17:37:29 +0100

    Migracja rozszerzajaca.

---
+++ PriceSafari/Migrations/20260303163657_newvaluesforallegroscrapedoffer.cs
@@ -0,0 +1,78 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PriceSafari.Migrations
+{
+    /// <inheritdoc />
+    public partial class newvaluesforallegroscrapedoffer : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "RatingCount",
+                table: "AllegroScrapedOffers",
+                type: "integer",
+                nullable: true);
+
+            migrationBuilder.AddColumn<double>(
+                name: "RatingPositivePercent",
+                table: "AllegroScrapedOffers",
+                type: "double precision",
+                nullable: true);
+
+            migrationBuilder.AddColumn<long>(
+                name: "StoreIdOnAllegro",
+                table: "AllegroScrapedOffers",
+                type: "bigint",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "RatingCount",
+                table: "AllegroPriceHistories",
+                type: "integer",
+                nullable: true);
+
+            migrationBuilder.AddColumn<double>(
+                name: "RatingPositivePercent",
+                table: "AllegroPriceHistories",
+                type: "double precision",
+                nullable: true);
+
+            migrationBuilder.AddColumn<long>(
+                name: "StoreIdOnAllegro",
+                table: "AllegroPriceHistories",
+                type: "bigint",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "RatingCount",
+                table: "AllegroScrapedOffers");
+
+            migrationBuilder.DropColumn(
+                name: "RatingPositivePercent",
+                table: "AllegroScrapedOffers");
+
+            migrationBuilder.DropColumn(
+                name: "StoreIdOnAllegro",
+                table: "AllegroScrapedOffers");
+
+            migrationBuilder.DropColumn(
+                name: "RatingCount",
+                table: "AllegroPriceHistories");
+
+            migrationBuilder.DropColumn(
+                name: "RatingPositivePercent",
+                table: "AllegroPriceHistories");
+
+            migrationBuilder.DropColumn(
+                name: "StoreIdOnAllegro",
+                table: "AllegroPriceHistories");
+        }
+    }
+}
