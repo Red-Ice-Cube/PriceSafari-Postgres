13cf3b86f49d78a4a9a6a6723108142c03713fd1
Mateusz Werner <matiwerner20@gmail.com>
2026-03-06 02:11:36 +0100

    Dodanie migracji rozszerzenia allegro exztended infos

---
+++ PriceSafari/Migrations/20260306011052_extendinfoidaonallegro.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PriceSafari.Migrations
+{
+    /// <inheritdoc />
+    public partial class extendinfoidaonallegro : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<long>(
+                name: "IdAllegro",
+                table: "AllegroPriceHistoryExtendedInfos",
+                type: "bigint",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IdAllegro",
+                table: "AllegroPriceHistoryExtendedInfos");
+        }
+    }
+}
