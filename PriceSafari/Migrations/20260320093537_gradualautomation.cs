15914c53302111b98ed1ffd887846a199722d267
Mateusz Werner <matiwerner20@gmail.com>
2026-03-20 10:48:41 +0100

    Wdrozenie noweych ustawien automatu

---
+++ PriceSafari/Migrations/20260320093537_gradualautomation.cs
@@ -0,0 +1,84 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PriceSafari.Migrations
+{
+    /// <inheritdoc />
+    public partial class gradualautomation : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "EnableGradualDecrease",
+                table: "AutomationRules",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "EnableGradualIncrease",
+                table: "AutomationRules",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<decimal>(
+                name: "GradualDecreaseValue",
+                table: "AutomationRules",
+                type: "numeric(18,2)",
+                nullable: false,
+                defaultValue: 0m);
+
+            migrationBuilder.AddColumn<decimal>(
+                name: "GradualIncreaseValue",
+                table: "AutomationRules",
+                type: "numeric(18,2)",
+                nullable: false,
+                defaultValue: 0m);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsGradualDecreasePercent",
+                table: "AutomationRules",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsGradualIncreasePercent",
+                table: "AutomationRules",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "EnableGradualDecrease",
+                table: "AutomationRules");
+
+            migrationBuilder.DropColumn(
+                name: "EnableGradualIncrease",
+                table: "AutomationRules");
+
+            migrationBuilder.DropColumn(
+                name: "GradualDecreaseValue",
+                table: "AutomationRules");
+
+            migrationBuilder.DropColumn(
+                name: "GradualIncreaseValue",
+                table: "AutomationRules");
+
+            migrationBuilder.DropColumn(
+                name: "IsGradualDecreasePercent",
+                table: "AutomationRules");
+
+            migrationBuilder.DropColumn(
+                name: "IsGradualIncreasePercent",
+                table: "AutomationRules");
+        }
+    }
+}
